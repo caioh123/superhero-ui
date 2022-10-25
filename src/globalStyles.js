c206fdba6f042f2f2c36007177f45f128e91e61e
caioh123 <caionwebdev@gmail.com>
2022-10-25 15:17:32 -0300

    feature/added-styled-components

---
+++ src/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+`;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./styles";
+
+export const Home = () => {
+  return <S.Container>oi</S.Container>;
+};
